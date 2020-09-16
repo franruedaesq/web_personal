6767bb02aa70f6f36054b2267880eb700503f9b7
Francisco Rueda <franruedaesq@gmail.com>
2020-09-16 20:59:10 -0300

    Updates

---
+++ precache-manifest.20acc10029ac20c1310559ecb3ea4479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae9f37cf15bad28725fd302871691a8",
+    "url": "/web_personal/index.html"
+  },
+  {
+    "revision": "b7f7297972975107b9dc",
+    "url": "/web_personal/static/css/main.8c907a64.chunk.css"
+  },
+  {
+    "revision": "a5e0d7badd8dfa302e24",
+    "url": "/web_personal/static/js/2.65deefcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web_personal/static/js/2.65deefcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f7297972975107b9dc",
+    "url": "/web_personal/static/js/main.97f8fe43.chunk.js"
+  },
+  {
+    "revision": "7163404341df965d584c",
+    "url": "/web_personal/static/js/runtime-main.83827c5c.js"
+  }
+]);
